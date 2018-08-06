2e9b49acef61d98effa52b583352a35e1ad65c76
OsmanOk <osman.okuyan@ogrenci.selcuk.edu.tr>
2018-08-06 20:52:32 +0300

    router data transfering and list item styling

--- src/container/issues.js
+++ src/container/issues.js
@@ -3,6 +3,7 @@ import firebase from 'firebase';
 import 'firebase/database';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
+import './issues.css';
 
 const issueStyle = {
   marginTop: '100px',
@@ -30,7 +31,8 @@ class Issues extends Component {
         issueHeader: snap.val().issueHeader,
         issueDate: snap.val().issueDate,
         issueMachineId: snap.val().issueMachineId,
-        issueCreator: snap.val().issueCreator
+        issueCreator: snap.val().issueCreator,
+        issueStatus: snap.val().issueStatus,
       })
 
       this.setState({
@@ -40,31 +42,39 @@ class Issues extends Component {
   }
 
 
-
   render(){
     return(
       <div>
         <div style={issueStyle} className="row">
         
         {
-          this.state.issues.map((issue) => {
+          this.state.issues.map((issues) => {
             return (
               <div id="issue-list-container" className="container">
                 <div id="issue-list" className="col-9">
                   <ul className="list-group">
-                    <li className="list-group-item">
-                      <div className="row">
-                        <Link
-                        to={`/issues/${issue.issueID}`}
-                        id="issue-list-item"
-                        issueID={issue.issueID}
-                        >
-                          deneme link 
-                        </Link>
-                        <div>
-                          {issue.issueHeader}
+                    <li className="list-group-item issue-list-item">
+                      <Link to={{
+                        pathname: `/issues/${issues.issueID}`,
+                        state: { issues }}}
+                        id="issue-list-item-link"
+                      >
+                        <div className="row">
+                          <div id="statusBtn" className="col-2">
+                            <button type="button" className="btn btn-danger ">Cozulmedi</button>
+                          </div>
+                          <div className="col-6 text-dark">
+                            <h2>{issues.issueHeader}</h2>
+                            <h6>{issues.issueCreator} tarafindan olusturuldu</h6>
+                          </div>
+                          <div className="col-4">
+                            <h6><span className="text-dark">Makine No: </span><span className="text-danger">{issues.issueMachineId}</span></h6>
+                            <div className="mt-4">
+                              <small className="text-dark pt-5">{issues.issueDate}</small>
+                            </div>
+                          </div>
                         </div>
-                      </div>
+                      </Link>
                     </li>
                   </ul>
                 </div>
--- src/container/issuesRouter.js
+++ src/container/issuesRouter.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
+import { Link } from 'react-router-dom';
 
 class IssuesRouter extends Component {
 
-  constructor(props){
-    super(props);
-    this.issueID = props.issueID;
+  constructor(){
+    super();
+
+    this.state = {
+      issueID: '',
+      issueHeader: '',
+      issue: '',
+      issueDate: '',
+      issueMachineId: '',
+      issueCreator: '',
+    }
   }
 
-  render() {
+  componentWillMount() {
+    
+    console.log(this.props);
 
+    const item = this.props.history.location.state.issues
+    
+    this.setState({issueID: item.issueID})
+    this.setState({issueHeader: item.issueHeader})
+    this.setState({issue: item.issue})
+    this.setState({issueDate: item.issueDate})
+    this.setState({issueMachineId: item.issueMachineId})
+    this.setState({issueCreator: item.issueCreator})
+    this.setState({issueStatus: item.issueStatus})
+  }
+
+
+  
+  render() {
     return (
       <div>
-        <h3>Deneme</h3>
-        {console.log(this.issueID)}
+        
       </div>
     );
   }
